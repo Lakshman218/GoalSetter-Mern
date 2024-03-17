ce9d44efc6da26d2b6a451555704109eb6a2b164
LAKSHMAN S <lakshmans218@gmail.com>
2024-03-17 17:56:58 +0530

    first commit

---
+++ backend/controllers/userController.js
@@ -0,0 +1,118 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+// register
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please add all fields')
+  }
+
+  // Check if user exists
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      profileUrl: user.profileUrl,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+})
+
+// login
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+
+  // Check for user email
+  const user = await User.findOne({ email })
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      profileUrl: user.profileUrl,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid credentials')
+  } 
+})
+
+// account
+const getMe = asyncHandler(async (req, res) => {
+  res.status(200).json(req.user)
+})
+
+//edit user
+const editUser=asyncHandler(async(req,res)=>{
+  const {userId,name,email}=req.body
+  const user=await User.findByIdAndUpdate(userId,{name,email},{new:true})
+
+  if(user){
+      res.status(200).json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        profileUrl: user.profileUrl,
+        token: req.token
+      })
+  }else{
+      res.status(404)
+      throw new Error('User not Found')
+  }
+})
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+}
+
+//photo url upload
+const profileUpload = asyncHandler(async (req, res) => {
+  const url = req.body.url;
+
+  const user = await User.findByIdAndUpdate(req.user.id, {
+    profileUrl: url
+  }, { new: true });
+
+  
+  res.status(200).json(user);
+});
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getMe,
+  editUser,
+  profileUpload
+}
---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { editUser, profileUpdate } from '../features/auth/authSlice';
+import { toast } from 'react-toastify';
+
+function Profile() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if(!user) {
+      navigate("/login")
+    }
+  }, [user, navigate, dispatch])
+
+  const [image, setImage] = useState('')
+
+  const uploadImage = (e) => {
+    e.preventDefault();
+
+    if(!image) {
+      toast.error('please upload a file')
+      return;
+    }
+
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "jesruipz");
+    data.append("cloud_name", "dpn5xsh8k");
+
+    fetch("https://api.cloudinary.com/v1_1/dpn5xsh8k/image/upload", {
+      method: "post",
+      body: data,
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+      dispatch(profileUpdate(data.url))
+    })
+    .catch((err) => console.log(err))
+  }
+
+  const handleEdit = (userId, name, email) => {
+    const newName = prompt("Enter new name:", name);
+    const newEmail = prompt("Enter new Email", email);
+    if(newName === null || newEmail === null) {
+      return;
+    }
+    if(newName && newEmail) {
+      dispatch(editUser({ userId, name:newName, email:newEmail }))
+    }
+  }
+
+  return (
+    <div>
+      <div>User Dashboard</div>
+      <div className="profile">
+        <div className="profile-image">
+        <img
+            src={
+              user?.profileUrl
+                ? user.profileUrl
+                : "https://static.vecteezy.com/system/resources/thumbnails/002/387/693/small/user-profile-icon-free-vector.jpg"
+            }
+            alt="profile"
+          />
+        </div>
+
+        <div className="profile-card">
+          <div className="profile-info">
+            <p>Name: {user && user.name} </p>
+            <p>Email: {user && user.email} </p>
+          </div>
+
+          <div className="profile-buttons">
+            {/* <button 
+              className='btn-1'
+              onClick={()=>handleEdit(user._id, user.name, user.email)}
+              >Edit Profile
+            </button> */}
+
+            <div className="upload-button">
+              <div class="custom-file-upload">
+                <label for="profile" class="custom-button">
+                  Choose File
+                </label>
+                <input
+                  onChange={(e) => setImage(e.target.files[0])}
+                  type="file"
+                  name="profile"
+                  id="profile"
+                  class="hidden-input"
+                />
+              </div>
+
+              <button className="btn" onClick={uploadImage}>
+                Upload!
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
